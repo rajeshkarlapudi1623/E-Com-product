66ab80b93ddecbc464bcf7476b6e3d8d6012fbae
Rajesh <rajeshkarlapudi1623@gmail.com>
2024-03-07 09:28:42 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Product from './components/Product';
+import './App.css'
+
+// For some reason, React didn't render when I use Context Provider
+// and it didn't show any errors on the console which was really confusing.
+// So I had to use global state manually by passing it down level by level,
+// but I will come back to refactor the code to useContext API when I figure out the problem
+function App() {
+	const [productQuantity, setProductQuantity] = useState(0);
+	const [cartProductQuantity, setCartProductQuantity] = useState(0);
+	return (
+		<>
+			<Navbar
+				cartProductQuantity={cartProductQuantity}
+				setCartProductQuantity={setCartProductQuantity}
+			/>
+			<Product
+				productQuantity={productQuantity}
+				setProductQuantity={setProductQuantity}
+				setCartProductQuantity={setCartProductQuantity}
+			/>
+		</>
+	);
+}
+
+export default App;
---
+++ src/data/images.js
@@ -0,0 +1,18 @@
+export const images = [
+	{
+		png: require('../assets/image-product-1.jpg'),
+		thumbnail: require('../assets/image-product-1-thumbnail.jpg'),
+	},
+	{
+		png: require('../assets/image-product-2.jpg'),
+		thumbnail: require('../assets/image-product-2-thumbnail.jpg'),
+	},
+	{
+		png: require('../assets/image-product-3.jpg'),
+		thumbnail: require('../assets/image-product-3-thumbnail.jpg'),
+	},
+	{
+		png: require('../assets/image-product-4.jpg'),
+		thumbnail: require('../assets/image-product-4-thumbnail.jpg'),
+	},
+];
